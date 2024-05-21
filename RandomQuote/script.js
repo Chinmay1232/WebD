54294a76249facffd2aa2d440db0be6886071fce
Chinmay1232 <74865931+Chinmay1232@users.noreply.github.com>
2024-05-21 21:52:18 +0530

    Added New Quote Button

--- RandomQuote/script.js
+++ RandomQuote/script.js
@@ -3,6 +3,7 @@ const quote = document.querySelector("#quote")
 const author = document.querySelector("#author")
 const loading= document.getElementsByClassName("animated-background")
 const heading= document.getElementById("heading")
+const btn = document.querySelector("button")
 
 
 const getQuote = async() => {
@@ -15,7 +16,34 @@ const getQuote = async() => {
     {
         loading[i].classList.add("hide")
     }
+    author.classList.remove("hide")
     heading.classList.remove("hide")
+    btn.classList.remove("hide")
 }
+
+btn.addEventListener("click", async() => {
+    loading[1].classList.remove("hide")
+    quote.classList.add("hide")
+
+    loading[2].classList.remove("hide")
+    author.classList.add("hide")
+
+    let get = await fetch(url)
+    let data = await get.json()
+    quote.innerHTML = data.content
+    author.innerHTML = data.author
+
+    for(let i = 0; i < loading.length; i++)
+    {
+        loading[i].classList.add("hide")
+    }
+
+    quote.classList.remove("hide");
+    author.classList.remove("hide");
+
+
+
+    
+})
 
 getQuote()
